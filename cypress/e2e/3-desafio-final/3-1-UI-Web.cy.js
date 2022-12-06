8ad98e57e8d2ab6727e69f59c7c95b222ea02083
Falande-L-Etienne <falande2512@gmail.com>
2022-12-06 20:55:25 -0300

    atualizado

--- cypress/e2e/3-desafio-final/3-1-UI-Web.cy.js
+++ cypress/e2e/3-desafio-final/3-1-UI-Web.cy.js
@@ -12,5 +12,15 @@ describe('Funcionalidade: Adicionar itens', () => {
         cy.AddItem()
         cy.get('.dropdown-toggle > .mini-cart-items').should('contain', 3)
     
-   }); 
+   });
+   
+   it.only('Não pode adicionar dois itens do mesmo produto', () => {
+
+      cy.visit('product/produto-teste-11')
+      cy.AddItem()
+      cy.visit('product/produto-teste-11')
+      cy.get('.single_add_to_cart_button').click()
+      cy.get('.woocommerce-error > li').should('contain', 'Você não pode adicionar outro "Produto Teste" em seu carrinho.')
+  
+   });
 });
